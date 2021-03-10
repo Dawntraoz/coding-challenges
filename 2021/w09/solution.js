95839b26e384db6c48d0db554447bdc1a47ca308
Dawntraoz <albasilventefu@gmail.com>
2021-03-10 13:42:04 +0100

    First solution but doesn't count

--- 2021/w09/solution.js
+++ 2021/w09/solution.js
@@ -1,7 +1,30 @@
 const rgb2Hex = (r, g, b) => {
-  // your solution here
+  const letter = {
+    0: '0',
+    1: '1',
+    2: '2',
+    3: '3',
+    4: '4',
+    5: '5',
+    6: '6',
+    7: '7',
+    8: '8',
+    9: '9',
+    10: 'A',
+    11: 'B',
+    12: 'C',
+    13: 'D',
+    14: 'E',
+    15: 'F'
+  }
 
-  return undefined;
+  const calcDigit = (rgbDigit) => {
+    if (Math.trunc(rgbDigit/16) > 15) return letter[15] + letter[15];
+    if (Math.sign(rgbDigit) === -1) return letter[0] + letter[0];
+    return letter[Math.trunc(rgbDigit/16)] + letter[(Math.abs(rgbDigit/16) - Math.floor(rgbDigit/16)) * 16]
+  }
+
+  return `#${calcDigit(r) + calcDigit(g) + calcDigit(b)}`;
 };
 
 module.exports = rgb2Hex;
